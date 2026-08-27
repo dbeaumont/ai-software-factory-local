b4f2ab558e924a25153a3705d461cbef081d4b10
beaumoda@harmonie.com <david.beaumont@ca-lf.com>
2026-08-27 09:11:11 +0200

    fix(litellm): correction passe de la variable  OPEN_AI_KEY sur le script de entrypoint.sh

--- infrastructure/litellm/entrypoint.sh
+++ infrastructure/litellm/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+export OPENAI_API_KEY="${OPENAI_API_KEY:-${VAULT_OPENAI_API_KEY:-}}"
+
 sed \
   -e "s|__OLLAMA_MODEL__|${OLLAMA_MODEL}|g" \
   -e "s|__OPENAI_MODEL__|${OPENAI_MODEL}|g" \
